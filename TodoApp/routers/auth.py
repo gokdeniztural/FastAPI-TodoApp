f0f783ca411798df36ab603b68efe9b7b82f074a
Gökdeniz Tural <tgkdnz@gmail.com>
2026-05-11 00:12:11 +0300

    Secure Todo endpoints to require authentication and user ownership

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -56,7 +56,7 @@ def authenticate_user(username: str, password: str, db):
 
 
 def create_access_token(username: str, user_id: int, expires_delta: timedelta):
-    encode = {'sub': username, 'id': user_id}
+    encode = {'sub': username, 'id': user_id} 
     expire = datetime.now(timezone.utc) + expires_delta
     encode.update({'exp': expire})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
