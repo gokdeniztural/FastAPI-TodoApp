1971d335bfc9cca1e4dc8b2e1be21e3d7f9990cb
Gökdeniz Tural <tgkdnz@gmail.com>
2026-05-18 19:14:08 +0300

    test: add test_todos.py with db and current user overrides and read_all test

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 
-from ..routers.todos import get_db
+from ..routers.todos import get_db, get_current_user
 from ..database import Base 
 from ..main import app
 
-
+from fastapi import status
+from fastapi.testclient import TestClient
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db" # test için sqlite kullanabiliriz. Production için
                                                   # postgresql kullanımımız devam ediyor.
@@ -27,8 +28,20 @@ def override_get_db():
     finally:
         db.close()
 
-
+def override_get_current_user():
+    return {'username': 'testuser', 'id': 1, 'user_role': 'admin'}
 
 app.dependency_overrides[get_db] = override_get_db
 # uygulama çalışırken get_db bağımlılığı istenilen yerlerde bu fonksiyonun çalışmasını sağlıyoruz. 
 # böylece test veritabanını kullanarak testlerimizi yapıyoruz !!!
+
+app.dependency_overrides[get_current_user] = override_get_current_user
+# uygulama çalışırken get_current_user bağımlılığı istenilen yerlerde bu fonksiyon kullanılır. JWT yok!!
+
+client = TestClient(app)
+
+def test_read_all_authenticated():
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK # 200 durum kodu dönmeli
+    assert response.json() == [] # Burada şuan testdb.db boş olduğu için boş liste dönmeli. İleride değişecek!!
+
