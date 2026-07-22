dd57f62f47e0c5285b6da2abbed42823ab47f784
Gökdeniz Tural <tgkdnz@gmail.com>
2026-07-23 00:14:19 +0300

    Code analysis and updates to outdated library versions were performed.

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 #SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:REDACTED@localhost:5432/TodoApplicationDatabase'
