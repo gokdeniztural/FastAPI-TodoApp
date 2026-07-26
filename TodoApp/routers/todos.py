f3d429334e0938d7ad32f5458765d50a642eb768
Gökdeniz Tural <tgkdnz@gmail.com>
2026-07-26 22:36:58 +0300

    feat(ui): integrate navbar, auth mechanism, and dynamic todo rendering via Jinja2

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -1,23 +1,20 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, HTTPException, Path
+from fastapi import APIRouter, Depends, HTTPException, Path, Request, status
 from starlette import status
 from ..models import Todos
 from ..database import SessionLocal
 from pydantic import BaseModel, Field
 from .auth import get_current_user
+from starlette.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
 
+templates = Jinja2Templates(directory="TodoApp/templates")
 
 router = APIRouter(
     prefix="/todos",
     tags=["todos"]
 )
-
-# models.Base.metadata.create_all(bind=engine) # Bu satır sadece todos.db mevcut değilse çalışacaktır! / Tablo oluşturuldu
-
-# (main.py) burada!
-
-# app.include_router(auth.router) # auth.py dosyasındaki router'ı main'e dahil ettiğimiz kısım.
 
 def get_db():
     db = SessionLocal()
@@ -36,6 +33,33 @@ class TodoRequest(BaseModel):
     priority: int = Field(gt=0, lt=6)
     complete: bool
 
+def redirect_to_login():
+    redirect_response = RedirectResponse(url="/auth/login-page", status_code=status.HTTP_302_FOUND)
+    # kullanıcıyı login sayfasına yönlendiriyoruz.
+    redirect_response.delete_cookie(key="access_token")  # Çerezi silmek için delete_cookie metodunu kullanıyoruz.
+    return redirect_response
+
+### Pages ###
+
+@router.get('/todo-page')
+async def render_todo_page(request: Request, db: db_dependency):
+    try:
+        user = await get_current_user(request.cookies.get("access_token"))
+
+        if user is None:
+            return redirect_to_login()
+        
+        todos = db.query(Todos).filter(Todos.owner_id == user.get('id')).all()
+
+        return templates.TemplateResponse(request=request, name="todo.html", context={"todos": todos, "user": user})
+
+    except:
+        return redirect_to_login()
+
+
+
+
+### Endpoints ###
 @router.get('/',status_code=status.HTTP_200_OK)
 async def read_all(user: user_dependency, db: db_dependency):
     if user is None:
